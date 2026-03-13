c903988b77c627ba17496ee3502a87fb36dd01cf
Taciano <taciolb@gmail.com>
2026-03-13 19:56:23 -0300

    adicionado client de email

---
+++ src/main/java/com/tlbtech/bffagendador/infrastructure/client/EmailClient.java
@@ -0,0 +1,11 @@
+package com.tlbtech.bffagendador.infrastructure.client;
+
+import com.tlbtech.bffagendador.business.dto.out.TarefasDTOResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "notificacao", url = "${notificacao.url}")
+public interface EmailClient {
+
+    void enviarEmail(@RequestBody TarefasDTOResponse dto);
+}
